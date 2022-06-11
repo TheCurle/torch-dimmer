bdb1502951c62066b1d58f215efdad372307f797
Curle <curle@gemwire.uk>
2022-06-11 21:56:05 +0100

    Fork: Let trapdoors emit light.

--- src/main/java/uk/gemwire/dimmer/DimmerMain.java
+++ src/main/java/uk/gemwire/dimmer/DimmerMain.java
@@ -3,6 +3,7 @@ package uk.gemwire.dimmer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.StateDefinition;
 import org.apache.logging.log4j.LogManager;
@@ -19,29 +20,20 @@ public class DimmerMain implements ModInitializer {
             Field lightEmission = BlockBehaviour.BlockStateBase.class.getDeclaredField("field_23167");
             lightEmission.setAccessible(true);
 
-            StateDefinition<?, ?> standingDef = (StateDefinition<?, ?>) stateDef.get(Blocks.REDSTONE_TORCH);
-            StateDefinition<?, ?> wallDef = (StateDefinition<?, ?>) stateDef.get(Blocks.REDSTONE_WALL_TORCH);
+            StateDefinition<?, ?> openDef = (StateDefinition<?, ?>) stateDef.get(Blocks.OAK_TRAPDOOR);
 
-            standingDef.getPossibleStates().forEach((state) -> {
+            openDef.getPossibleStates().forEach((state) -> {
                 try {
-                    lightEmission.set(state, 0);
+                    if (state.getValue(TrapDoorBlock.OPEN))
+                        lightEmission.set(state, 15);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
             });
 
-            wallDef.getPossibleStates().forEach((state) -> {
-                try {
-                    lightEmission.set(state, 0);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            });
+            stateDef.set(Blocks.OAK_TRAPDOOR, openDef);
 
-            stateDef.set(Blocks.REDSTONE_TORCH, standingDef);
-            stateDef.set(Blocks.REDSTONE_WALL_TORCH, wallDef);
-
-            LogManager.getLogger("dimmer").info("Restone Torch Luminance fixed.");
+            LogManager.getLogger("dimmer").info("Oak Trapdoor Luminance fixed.");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
